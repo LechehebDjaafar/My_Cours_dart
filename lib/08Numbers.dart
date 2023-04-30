c7e8ea4b216365e30c1ed3ba4ec02915d5e795e3
LechehebDjaafar <djaafardjaafarlolo@gmail.com>
2023-04-30 10:58:37 +0100

    Cours_Dart

--- lib/08Numbers.dart
+++ lib/08Numbers.dart
@@ -1,3 +1,13 @@
 void main(){
-  
+  int a = 12;
+  int b = 0;
+  int c = -224113;
+  int d = -2;
+  print("a is ${a.isFinite} ,b is ${b.isFinite} ,c is ${c.isFinite} ,d is ${d.isFinite}");
+  print("a is ${a.isInfinite} ,b is ${b.isInfinite} ,c is ${c.isInfinite} ,d is ${d.isInfinite}");
+  print("a is ${a.isNegative} ,b is ${b.isNegative} ,c is ${c.isNegative} ,d is ${d.isNegative}");
+  print("a is ${a.sign} ,b is ${b.sign} ,c is ${c.sign} ,d is ${d.sign}");
+  print("a is ${a.isEven} ,b is ${b.isEven} ,c is ${c.isEven} ,d is ${d.isEven}");
+  print("a is ${a.abs()} ,b is ${b.abs()} ,c is ${c.abs()} ,d is ${d.abs()}");
+
 }
